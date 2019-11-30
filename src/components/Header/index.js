af3f6e8c748c623e30974125ab8b8359a89ffa2f
Leonardo Barbosa <leonardo.pb@icloud.com>
2019-11-30 15:07:24 -0300

    Added dashboard styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import logo from '~/assets/logo-purple.svg';
@@ -7,7 +8,9 @@ import Notifications from '~/components/Notifications';
 
 import { Container, Content, Profile } from './styles';
 
-export default function components() {
+export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -21,11 +24,14 @@ export default function components() {
 
           <Profile>
             <div>
-              <strong>Leonardo Barbosa</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu Perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
               alt="Df"
             />
           </Profile>
